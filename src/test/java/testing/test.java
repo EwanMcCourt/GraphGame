f985cf929a32fdc22ec80afa03b8072f81384c9f
tobyweir3 <BreakAt32>
2023-03-28 11:16:02 +0100

    added testing, changed pom file

---
+++ src/test/java/testing/test.java
@@ -0,0 +1,13 @@
+package testing;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class test {
+    @Test
+    public void testSomething() {
+        assertEquals(2, 1 + 1);
+    }
+}

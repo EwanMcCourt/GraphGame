f9c6e3fbc1890e98e8961752e3d132f55b5006c3
Euan McAllister SE2020 <euan.mcallister.2020@uni.strath.ac.uk>
2023-03-31 17:45:05 +0100

    Wrote basic test for player.equals

--- src/test/java/testing/testPlayer.java
+++ src/test/java/testing/testPlayer.java
@@ -1,4 +1,15 @@
 package testing;
+import MVC.Model.Player;
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+
 public class testPlayer {
+    @Test
+    public void TestEquals(){
+        Player testPlayer = new Player("test", 0);
+        assertEquals( testPlayer.equals(testPlayer) ,true );
+        Player testPlayer2 = new Player("test2", 4);
+        assertEquals( testPlayer.equals(testPlayer2) ,false );
+    }
 }

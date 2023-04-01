055b882945c6e5cd25d3b7345c974bc5193c1259
Euan McAllister SE2020 <euan.mcallister.2020@uni.strath.ac.uk>
2023-04-01 14:38:32 +0100

    Player method tests

--- src/test/java/testing/testPlayer.java
+++ src/test/java/testing/testPlayer.java
@@ -5,11 +5,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class testPlayer {
+    Player testPlayer = new Player("test", 0);
+    Player testPlayer2 = new Player("test2", 0);
     @Test
     public void TestEquals(){
-        Player testPlayer = new Player("test", 0);
+
         assertEquals( testPlayer.equals(testPlayer) ,true );
-        Player testPlayer2 = new Player("test2", 4);
         assertEquals( testPlayer.equals(testPlayer2) ,false );
+
+
+    }
+    @Test
+    public void TestGetUsername() {
+        assertEquals(testPlayer.getUsername(), "test");
+
+    }
+    @Test
+    public void TestGetGamesPlayed() {
+        assertEquals(testPlayer.getGamesPlayed(), 0);
+    }
+    public void TestToString() {
+        assertEquals(testPlayer.toString(), "test 0");
     }
 }

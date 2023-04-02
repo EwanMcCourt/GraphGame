e7cb23de188b69facdfe0986e5568328acfd0432
Cathal <cathal.innes.2016@uni.strath.ac.uk>
2023-04-02 20:58:54 +0100

    Removed getWeight from GraphADT and change Dijkstra to not specify list type

--- src/main/java/Graph/GraphADT.java
+++ src/main/java/Graph/GraphADT.java
@@ -5,7 +5,6 @@ import java.util.Set;
 public interface GraphADT<N extends Node<N>> {
     void addNode(N node);
     void addConnection(N source, N target, int weight, Boolean bidirectional);
-    double getWeight(N source, N target);
     N getNode(int index);
     Set<N> getNodes();
 }

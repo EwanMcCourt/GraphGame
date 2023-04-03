5e3266856f69d1c316e68313251a4d7812101ee7
Cathal <cathal.innes.2016@uni.strath.ac.uk>
2023-04-03 21:01:08 +0100

    Fixed tests for Dijkstra and StepGraph

--- src/test/java/testing/testDijkstra.java
+++ src/test/java/testing/testDijkstra.java
@@ -2,8 +2,6 @@ package testing;
 
 import Graph.ALGraph;
 import Graph.Dijkstra;
-import Graph.GraphADT;
-import Graph.StepGraph;
 import MVC.Model.Point;
 import org.junit.jupiter.api.Test;
 
@@ -18,12 +16,12 @@ public class testDijkstra {
         Point point2 = new Point(2);
         Point point3 = new Point(3);
         Point point4 = new Point(4);
-        ALGraph<Point> graph = new ALGraph<Point>();
+        ALGraph<Point> graph = new ALGraph<>();
         graph.addConnection(point1, point2, 100, true);
         graph.addConnection(point2, point4, 1, false);
         graph.addConnection(point1, point3, 2, true);
         graph.addConnection(point3, point4, 100, false);
-        Dijkstra<Point> test = new Dijkstra<Point>(point1,graph, false);
+        Dijkstra<Point> test = new Dijkstra<>(point1,graph, false);
         assertEquals(test.getGraphPath(point4).getNodes().get(0).getIndex(), 1);
         assertEquals(test.getGraphPath(point4).getNodes().get(1).getIndex(), 2);
         assertEquals(test.getGraphPath(point4).getNodes().get(2).getIndex(), 4);
@@ -34,14 +32,20 @@ public class testDijkstra {
         Point point2 = new Point(2);
         Point point3 = new Point(3);
         Point point4 = new Point(4);
-        ALGraph<Point> graph = new ALGraph<Point>();
-        graph.addConnection(point1, point2, 100, true);
-        graph.addConnection(point2, point4, 1, false);
-        graph.addConnection(point1, point3, 2, true);
-        graph.addConnection(point3, point4, 100, false);
-        Dijkstra<Point> test = new Dijkstra<Point>(point1,graph, true);
-        assertEquals(test.getGraphPath(point4).getNodes().get(0).getIndex(), 1);
-        assertEquals(test.getGraphPath(point4).getNodes().get(1).getIndex(), 2);
-        assertEquals(test.getGraphPath(point4).getNodes().get(2).getIndex(), 4);
+        Point point5 = new Point(5);
+        ALGraph<Point> graph = new ALGraph<>();
+        graph.addConnection(point1, point2, 2, true);
+        graph.addConnection(point2, point3, 3, false);
+        graph.addConnection(point1, point4, 200, true);
+        graph.addConnection(point3, point4, 3, false);
+        graph.addConnection(point4, point5, 3, true);
+        Dijkstra<Point> test = new Dijkstra<>(point1,graph, true);
+        for (Point point : test.getGraphPath(point3).getNodes()) {
+            System.out.println(point.getIndex());
+        }
+
+        assertEquals(test.getGraphPath(point5).getNodes().get(0).getIndex(), 1);
+        assertEquals(test.getGraphPath(point5).getNodes().get(1).getIndex(), 4);
+        assertEquals(test.getGraphPath(point5).getNodes().get(2).getIndex(), 5);
     }
 }
--- src/test/java/testing/testStepGraph.java
+++ src/test/java/testing/testStepGraph.java
@@ -7,15 +7,16 @@ import MVC.Model.Point;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class testStepGraph {
     @Test
     public void testGetMaxLength(){
         Point point1 = new Point(1);
-        ALGraph<Point> ALgraph = new ALGraph<Point>();
-        StepGraph<Point> graph1 = new StepGraph<Point>(ALgraph);
+        ALGraph<Point> ALgraph = new ALGraph<>();
+        StepGraph<Point> graph1 = new StepGraph<>(ALgraph);
         ALgraph.addNode(point1);
-        StepGraph<Point> graph2 = new StepGraph<Point>(ALgraph);
+        StepGraph<Point> graph2 = new StepGraph<>(ALgraph);
         int result = graph1.getMaxLength();
         assertEquals(result, 0);
         result = graph2.getMaxLength();
@@ -23,30 +24,30 @@ public class testStepGraph {
     }
     @Test //not working rn: runs forever
     public void testGetPathBySizeWhenEmpty(){
-        ALGraph<Point> ALgraph = new ALGraph<Point>();
-        StepGraph<Point> graph1 = new StepGraph<Point>(ALgraph);
+        ALGraph<Point> ALgraph = new ALGraph<>();
+        StepGraph<Point> graph1 = new StepGraph<>(ALgraph);
         GraphPath<Point> result = graph1.getPathBySize(1);
-        System.out.println(result);
+        assertNull(result);
     }
     @Test
     public void testGetPathBySize(){
-        Point point1 = new Point(1);
-        Point point2 = new Point(2);
-        Point point10 = new Point(10);
+        Point p1 = new Point(1);
+        Point p2 = new Point(2);
+        Point p10 = new Point(10);
         ALGraph<Point> ALgraph = new ALGraph<>();
-        ALgraph.addNode(point1);
-        ALgraph.addNode(point2);
-        ALgraph.addNode(point10);
-        ALgraph.addConnection(point1, point2, 10, false);
-        ALgraph.addConnection(point2, point10, 15, true);
+        ALgraph.addNode(p1);
+        ALgraph.addNode(p2);
+        ALgraph.addNode(p10);
+        ALgraph.addConnection(p1, p2, 10, false);
+        ALgraph.addConnection(p2, p10, 15, true);
         StepGraph<Point> graph = new StepGraph<>(ALgraph);
         GraphPath<Point> result = graph.getPathBySize(3);
         System.out.println(result.getNodes());
         System.out.println(result.getNodes().get(0).getIndex());
         System.out.println(result.getNodes().get(1).getIndex());
         System.out.println(result.getNodes().get(2).getIndex());
-        assertEquals(result.getNodes().get(2).getIndex(), 1);
+        assertEquals(result.getNodes().get(0).getIndex(), 1);
         assertEquals(result.getNodes().get(1).getIndex(), 2);
-        assertEquals(result.getNodes().get(0).getIndex(), 10);
+        assertEquals(result.getNodes().get(2).getIndex(), 10);
     }
 }
